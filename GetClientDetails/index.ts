61bc037dc2e05330d087720925f908e80206e0e9
ShaheerAhmadCh1 <shaheer.ashraf@live.com>
2024-04-01 22:53:12 +0500

    -

--- GetClientDetails/index.ts
+++ GetClientDetails/index.ts
@@ -1,6 +1,6 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
-export class IpAddressPCF implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+export class GetClientDetails implements ComponentFramework.StandardControl<IInputs, IOutputs> {
     private ipAddressElement: HTMLParagraphElement;
     private _notifyOutputChanged: () => void;
 
